7b15585179e80dc479e10978fbb752cbf9e82a57
Nils <deele@tuta.io>
2018-11-22 11:18:25 +0200

    Rename ChargeDesk Customer Resource

--- lib/ChargeDesk/Customer.php
+++ lib/ChargeDesk/Customer.php
@@ -1,15 +1,16 @@
 <?php
+namespace ChargeDesk;
+
 /**
  * ChargeDesk Customer Resource
  */
-
-class ChargeDesk_Customer extends ChargeDesk_Resource {
+class Customer extends Resource {
 
 	/**
 	 * Creates a new Resource
 	 * @param array $data Fields to create a new charge with
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Created resource
+	 * @return Resource Created resource
 	 */
 	public static function create($data = array(), $api_key = null) {
 		return self::_post(get_class(), false, $data, $api_key);
@@ -19,7 +20,7 @@ class ChargeDesk_Customer extends ChargeDesk_Resource {
 	 * Retrieves an existing Resource
 	 * @param $key Resource key to retrieve
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource matching the provided $key
+	 * @return Resource Resource matching the provided $key
 	 */
 	public static function retrieve($key, $api_key = null) {
 		return self::_get(get_class(), $key, $api_key);
@@ -29,7 +30,7 @@ class ChargeDesk_Customer extends ChargeDesk_Resource {
 	 * @param $key Resource key to update
 	 * @param array $data Fields to update an existing resource with
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource with updated fields
+	 * @return Resource Resource with updated fields
 	 */
 	public static function update($key, $data = array(), $api_key = null) {
 		return self::_post(get_class(), $key, $data, $api_key);
@@ -39,7 +40,7 @@ class ChargeDesk_Customer extends ChargeDesk_Resource {
 	 * Deletes an existing Resource
 	 * @param $key Resource key to delete
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource matching the provided $key
+	 * @return Resource Resource matching the provided $key
 	 */
 	public static function delete($key, $data = array(), $api_key = null) {
 		return self::_delete(get_class(), $key, $data, $api_key);
@@ -49,7 +50,7 @@ class ChargeDesk_Customer extends ChargeDesk_Resource {
      * Find one or more existing resources
      * @param array $data Fields to search for existing resources
      * @param string $api_key API key to use for this request
-     * @return array of ChargeDesk_Resource Resource matching the provided $data array
+     * @return array of Resource Resource matching the provided $data array
      */
     public static function find($data = array(), $api_key = null) {
         return self::_get(get_class(), $data, $api_key);
@@ -59,10 +60,10 @@ class ChargeDesk_Customer extends ChargeDesk_Resource {
      * Gets a snapshot of the billing history for a customer.
      * @param array $data Fields to search for existing resources
      * @param string $api_key API key to use for this request
-     * @return array of ChargeDesk_Resource Resource matching the provided $data array
+     * @return array of Resource Resource matching the provided $data array
      */
     public static function history($data = array(), $api_key = null) {
         return self::_request("array", "get", self::_buildPath(get_class(), "history"), $data, $api_key);
     }
 }
-?>
+class_alias('ChargeDesk\Customer', 'ChargeDesk_Customer');
