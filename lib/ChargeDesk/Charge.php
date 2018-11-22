f0d78a0831f1525b2d2769bf942ab9e6713e6f93
Nils <deele@tuta.io>
2018-11-22 11:16:52 +0200

    Rename ChargeDesk Charge Resource

--- lib/ChargeDesk/Charge.php
+++ lib/ChargeDesk/Charge.php
@@ -1,15 +1,17 @@
 <?php
+namespace ChargeDesk;
+
 /**
  * ChargeDesk Charge Resource
  * Create, Update, Retrieve and Find Charges on ChargeDesk
  */
-class ChargeDesk_Charge extends ChargeDesk_Resource {
+class Charge extends Resource {
 
 	/**
 	 * Creates a new Resource
 	 * @param array $data Fields to create a new charge with
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Created resource
+	 * @return Resource Created resource
 	 */
 	public static function create($data = array(), $api_key = null) {
 		return self::_post(get_class(), false, $data, $api_key);
@@ -19,7 +21,7 @@ class ChargeDesk_Charge extends ChargeDesk_Resource {
 	 * Retrieves an existing Resource
 	 * @param $key Resource key to retrieve
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource matching the provided $key
+	 * @return Resource Resource matching the provided $key
 	 */
 	public static function retrieve($key, $api_key = null) {
 		return self::_get(get_class(), $key, $api_key);
@@ -29,7 +31,7 @@ class ChargeDesk_Charge extends ChargeDesk_Resource {
 	 * @param $key Resource key to update
 	 * @param array $data Fields to update an existing resource with
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource with updated fields
+	 * @return Resource Resource with updated fields
 	 */
 	public static function update($key, $data = array(), $api_key = null) {
 		return self::_post(get_class(), $key, $data, $api_key);
@@ -50,7 +52,7 @@ class ChargeDesk_Charge extends ChargeDesk_Resource {
 	 * @param $key Resource key to update
 	 * @param array $data Fields to update an existing resource with
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource with updated fields
+	 * @return Resource Resource with updated fields
 	 */
 	public static function refund($key, $data = array(), $api_key = null) {
 		return self::_post(get_class(), array($key, "refund"), $data, $api_key);
@@ -61,10 +63,10 @@ class ChargeDesk_Charge extends ChargeDesk_Resource {
 	 * @param $key Resource key to update
 	 * @param array $data Fields to update an existing resource with
 	 * @param string $api_key API key to use for this request
-	 * @return ChargeDesk_Resource Resource with updated fields
+	 * @return Resource Resource with updated fields
 	 */
 	public static function email($key, $data = array(), $api_key = null) {
 		return self::_post(get_class(), array($key, "email"), $data, $api_key);
 	}
 }
-?>
+class_alias('ChargeDesk\Charge', 'ChargeDesk_Charge');
