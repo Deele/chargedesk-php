aa8dde67961865cf4eeebdb3b765c079bf044512
Nils <deele@tuta.io>
2018-11-22 15:09:32 +0200

    Fix phpdoc for functions

--- lib/ChargeDesk/Charge.php
+++ lib/ChargeDesk/Charge.php
@@ -16,6 +16,9 @@ class Charge extends Resource
      * @param array $data Fields to create a new charge with
      * @param string $api_key API key to use for this request
      * @return Resource Created resource
+     * @throws ConnectError
+     * @throws RateLimitError
+     * @throws RequestError
      */
     public static function create($data = array(), $api_key = null)
     {
@@ -27,6 +30,9 @@ class Charge extends Resource
      * @param $key Resource key to retrieve
      * @param string $api_key API key to use for this request
      * @return Resource Resource matching the provided $key
+     * @throws ConnectError
+     * @throws RateLimitError
+     * @throws RequestError
      */
     public static function retrieve($key, $api_key = null)
     {
@@ -38,6 +44,9 @@ class Charge extends Resource
      * @param array $data Fields to update an existing resource with
      * @param string $api_key API key to use for this request
      * @return Resource Resource with updated fields
+     * @throws ConnectError
+     * @throws RateLimitError
+     * @throws RequestError
      */
     public static function update($key, $data = array(), $api_key = null)
     {
@@ -49,6 +58,9 @@ class Charge extends Resource
      * @param array $data Fields to search for existing resources
      * @param string $api_key API key to use for this request
      * @return array of ChargeDesk_Resource Resource matching the provided $data array
+     * @throws ConnectError
+     * @throws RateLimitError
+     * @throws RequestError
      */
     public static function find($data = array(), $api_key = null)
     {
@@ -61,6 +73,9 @@ class Charge extends Resource
      * @param array $data Fields to update an existing resource with
      * @param string $api_key API key to use for this request
      * @return Resource Resource with updated fields
+     * @throws ConnectError
+     * @throws RateLimitError
+     * @throws RequestError
      */
     public static function refund($key, $data = array(), $api_key = null)
     {
@@ -73,6 +88,9 @@ class Charge extends Resource
      * @param array $data Fields to update an existing resource with
      * @param string $api_key API key to use for this request
      * @return Resource Resource with updated fields
+     * @throws ConnectError
+     * @throws RateLimitError
+     * @throws RequestError
      */
     public static function email($key, $data = array(), $api_key = null)
     {
