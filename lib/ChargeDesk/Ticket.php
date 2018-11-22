c58283d0a107b36512265a413f0e9eb13a47b049
Nils <deele@tuta.io>
2018-11-22 14:43:33 +0200

    Rename ChargeDesk Ticket Handler

--- lib/ChargeDesk/Ticket.php
+++ lib/ChargeDesk/Ticket.php
@@ -1,9 +1,8 @@
 <?php
+namespace ChargeDesk;
+
 /**
  * ChargeDesk Ticket Handler
  */
-
-class ChargeDesk_Ticket extends ChargeDesk_Resource {
-
-}
-?>
+class Ticket extends Resource {}
+class_alias('ChargeDesk\Ticket', 'ChargeDesk_Ticket');
