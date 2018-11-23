ee27c9d62db0e2b3958805665764980fbdfd192a
Nils <deele@tuta.io>
2018-11-23 15:42:48 +0200

    Format bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -1,37 +1,41 @@
 <?php
+
 /**
  * Bootstrap ChargeDesk loading
  * Does not use autoloader for PHP 5.2 compatibility
+ *
+ * @package ChargeDesk
  */
-class ChargeDesk_Loader {
-	private $folder = "ChargeDesk";
-	private $files = array(
-		// Core singleton
-		"ChargeDesk",
-		// Model classes
-		"ChargeDesk_Resource",
-		"ChargeDesk_Charge",
-		"ChargeDesk_Customer",
-		"ChargeDesk_Product",
-		"ChargeDesk_Subscription",
-		"ChargeDesk_Ticket",
-		// Errors
-		"ChargeDesk_Error",
-		"ChargeDesk_ConnectError",
-		"ChargeDesk_RequestError",
-		"ChargeDesk_RateLimitError",
-		// Utilities
-		"ChargeDesk_Request",
-	);
+class ChargeDesk_Loader
+{
+    private $folder = "ChargeDesk";
+    private $files = array(
+        // Core singleton
+        "ChargeDesk",
+        // Model classes
+        "ChargeDesk_Resource",
+        "ChargeDesk_Charge",
+        "ChargeDesk_Customer",
+        "ChargeDesk_Product",
+        "ChargeDesk_Subscription",
+        "ChargeDesk_Ticket",
+        // Errors
+        "ChargeDesk_Error",
+        "ChargeDesk_ConnectError",
+        "ChargeDesk_RequestError",
+        "ChargeDesk_RateLimitError",
+        // Utilities
+        "ChargeDesk_Request",
+    );
 
-	public function build() {
-		foreach($this->files as $file) {
-			require(dirname(__FILE__)."/".$this->folder."/".$file.".php");
-		}
-	}
+    public function build()
+    {
+        foreach ($this->files as $file) {
+            require(dirname(__FILE__) . "/lib/" . $this->folder . "/" . $file . ".php");
+        }
+    }
 }
 
 // Include all files
 $loader = new ChargeDesk_Loader();
 $loader->build();
-?>
